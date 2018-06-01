0308944a189b671bc49ced55c226ba9a1f077290
edwardwang9008 <32474081+edwardwang9008@users.noreply.github.com>
2018-06-01 21:06:19 +0800

    周焯彬_上传角色面板文件_6月1号21:00
    上传角色面板文件

---
+++ CODN_UNKOWN/Assets/UI_Interface/Scripsts/Common/Const.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Const  {
+
+    public const float OriginWidth = 1920f;
+    public const float OriginHeight = 1080f;
+
+}
